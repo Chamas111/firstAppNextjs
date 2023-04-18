8494987b28399e7c844c5e5c891c1eb4ad25914d
Chamas111 <ahmad.chamas87@gmail.com>
2023-04-18 13:36:27 +0200

    some edit

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Error() {
   return (
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -10,15 +11,15 @@ export default function Error() {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <Link
             href="/"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a href="#" class="text-sm font-semibold text-gray-900">
+          </Link>
+          <Link href="#" class="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
